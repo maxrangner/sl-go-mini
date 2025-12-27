6ed0cb54665d2312a007df4536ab2137d4b0f0a5
maxrangner <max@rangner.com>
2025-12-28 00:37:49 +0100

    Work on system design.

--- src/SystemManager.cpp
+++ src/SystemManager.cpp
@@ -6,37 +6,70 @@ SystemManager::SystemManager() {
 
 void SystemManager::init() {
     Serial.println("SystemManager init()");
+
     _Button = _ButtonManager.addButton(BUTTON_PIN, true);
     Matrix.init();
-    // Tasks
-    Serial.print("Initializing on core: "); Serial.println(xPortGetCoreID());
-    xTaskCreatePinnedToCore(
-      uiTask, /* Function to implement the task */
-      "uiTask", /* Name of the task */
-      10000,  /* Stack size in words */
-      this,  /* Task input parameter */
-      0,  /* Priority of the task */
-      &uiTaskHandle,  /* Task handle. */
-      0 /* Core where the task should run */
+
+    dataQueue = xQueueCreate(QUEUE_LEN, sizeof(num));
+    stateQueue = xQueueCreate(QUEUE_LEN, sizeof(num));
+    dataQueue = xQueueCreate(QUEUE_LEN, sizeof(num));
+
+    if (dataQueue != nullptr) {
+        Serial.println("Queue created!");
+    }
+    Serial.print("Initializing SystemManager on core: "); Serial.println(xPortGetCoreID()); 
+    
+    xTaskCreatePinnedToCore(     // SystemManager Task
+      systemManagerTask,         // Function to implement the task
+      "systemManagerTask",       // Name of the task
+      4096,                      // Stack size in words
+      this,                      // Task input parameter
+      2,                         // Priority of the task
+      &systemManagerTaskHandle,  // Task handle.
+      0                          // Core where the task should run
     ); 
-    xTaskCreatePinnedToCore(
+    
+    xTaskCreatePinnedToCore(     // UI Task
+      uiTask,
+      "uiTask",
+      4096,
+      this, 
+      1,
+      &uiTaskHandle,
+      0
+    ); 
+
+    xTaskCreatePinnedToCore( // Data Task
       dataTask,
       "dataTask",
-      10000,
+      8192,
       this,
-      1,
+      0,
       &dataTaskHandle,
       1
     ); 
 }
 
+void SystemManager::systemManagerTask(void* pvParameters) {
+    Serial.print("systemmanagerTask running on core ");
+    Serial.println(xPortGetCoreID());
+
+    SystemManager* self = static_cast<SystemManager*>(pvParameters);
+    BaseType_t queueReturnStatus;
+
+    while(1) {
+        // CODE
+        vTaskDelay(pdMS_TO_TICKS(1));
+    } 
+}
+
 void SystemManager::uiTask(void* pvParameters) {
-    delay(200);
+    vTaskDelay(200);
     Serial.print("uiTask running on core ");
     Serial.println(xPortGetCoreID());
 
     SystemManager* self = static_cast<SystemManager*>(pvParameters);
-
+    BaseType_t queueReturnStatus;
     while(1) {
         self->_ButtonManager.updateAll();
         if (self->_Button->wasPushed()) {
@@ -47,6 +80,11 @@ void SystemManager::uiTask(void* pvParameters) {
             self->Matrix.changeColors();
             Serial.print("Button held on core: "); Serial.println(xPortGetCoreID());
         }
+        queueReturnStatus = xQueueReceive(self->dataQueue, (void *)&self->receiveNum, sizeof(num));
+        if (queueReturnStatus == pdTRUE) {
+            Serial.print("Data received on core: "); Serial.print(xPortGetCoreID());
+            Serial.print(" : "); Serial.println(self->receiveNum);
+        }
         vTaskDelay(1);
     }
 }
@@ -56,9 +94,17 @@ void SystemManager::dataTask(void* pvParameters) {
     Serial.println(xPortGetCoreID());
 
     SystemManager* self = static_cast<SystemManager*>(pvParameters);
+    BaseType_t queueReturnStatus;
 
     while(1) {
-        Serial.print("Hi from core: "); Serial.println(xPortGetCoreID());
+        queueReturnStatus = xQueueSend(self->dataQueue, (void*)&self->num, sizeof(num));
+        if (queueReturnStatus == pdTRUE) {
+            Serial.print("Data sent from core: "); Serial.print(xPortGetCoreID());
+            Serial.print(" : "); Serial.println(self->num);
+            self->num++;
+        } else {
+            Serial.print("Failed to send data from core: "); Serial.println(xPortGetCoreID());
+        }
         vTaskDelay(pdMS_TO_TICKS(1000));
     } 
 }
--- src/SystemManager.h
+++ src/SystemManager.h
@@ -1,16 +1,36 @@
 #pragma once
+#include "freertos.h"
+#include "WiFi.h"
 #include "GawiButtons.h"
 #include "LedMatrix.h"
+
+enum class SystemState {
+    NOT_CONNECTED,       // 0
+    CONNECTED,           // 1
+    SETUP                // 2
+};
 
 class SystemManager {
     Button* _Button;
     ButtonManager _ButtonManager;
     LedMatrix Matrix;
+    SystemState _State = SystemState::NOT_CONNECTED;
+    uint8_t num = 0;
+    uint8_t receiveNum = 0;
+
+    // Tasks
+    TaskHandle_t systemManagerTaskHandle = nullptr;
     TaskHandle_t uiTaskHandle = nullptr;
     TaskHandle_t dataTaskHandle = nullptr;
+
+    // Queues
+    QueueHandle_t dataQueue = nullptr;
+    QueueHandle_t stateQueue = nullptr;
+    QueueHandle_t departureQueue = nullptr;
 public:
     SystemManager();
     void init();
+    static void systemManagerTask(void* pvParameters);
     static void uiTask(void* pvParameters);
     static void dataTask(void* pvParameters);
 };
